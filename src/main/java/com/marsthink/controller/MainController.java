857425040a4f936aec4f811a1193326c4a25f864
zhoumao <zhoumao@meituan.com>
2016-12-20 16:41:45 +0800

    Add surport for insert data to frontend.
    Signed-off-by: zhoumao <zhoumao@meituan.com>

--- src/main/java/com/marsthink/controller/MainController.java
+++ src/main/java/com/marsthink/controller/MainController.java
@@ -3,6 +3,11 @@ package com.marsthink.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.ModelAndView;
+
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
 
 /**
  * Created by zhoumao on 2016/12/9.
@@ -14,4 +19,19 @@ public class MainController {
         return "index";
     }
 
+    @RequestMapping(value = "/msg", method = RequestMethod.GET)
+    public ModelAndView msg() {
+        String message = "消息1";
+        User user = new User("张三", 12, new Date());
+        List<User> us= new ArrayList<User>();
+        us.add(new User("张三", 12, new Date()));
+        us.add(new User("张四", 13, new Date()));
+        us.add(new User("张五", 14, new Date()));
+        ModelAndView mad = new ModelAndView("msg");
+        //将数据存入modelMap
+        mad.addObject("message", message);
+        mad.addObject(user);//默认为类名的首字母小写
+        mad.addObject("users", us);
+        return mad;
+    }
 }
---
+++ src/main/java/com/marsthink/controller/User.java
@@ -0,0 +1,42 @@
+package com.marsthink.controller;
+
+import java.util.Date;
+
+/**
+ * Created by zhoumao on 2016/12/20.
+ */
+public class User {
+    private String name;
+    private int age;
+    private Date date;
+
+    public User(String name, int age, Date date) {
+        this.name = name;
+        this.age = age;
+        this.date = date;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public Date getDate() {
+        return date;
+    }
+
+    public void setDate(Date date) {
+        this.date = date;
+    }
+}
